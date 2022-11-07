c88b76159aba98cd9158568bd903b68381226b08
sanjay9867 <45529353+sanjay9867@users.noreply.github.com>
2022-11-08 00:14:43 +0530

    Update nvdec.cpp
    New

--- app/src/main/cpp/skyline/soc/host1x/classes/nvdec.cpp
+++ app/src/main/cpp/skyline/soc/host1x/classes/nvdec.cpp
@@ -4,10 +4,42 @@
 #include "nvdec.h"
 
 namespace skyline::soc::host1x {
-    NvDecClass::NvDecClass(std::function<void()> opDoneCallback)
-        : opDoneCallback(std::move(opDoneCallback)) {}
+#define NVDEC_REG_INDEX(field_name)                                                                \
+    (offsetof(NvdecCommon::NvdecRegisters, field_name) / sizeof(u64))
 
-    void NvDecClass::CallMethod(u32 method, u32 argument) {
-        Logger::Warn("Unknown NVDEC class method called: 0x{:X} argument: 0x{:X}", method, argument);
+Nvdec::Nvdec(Host1x& host1x_)
+    : host1x(host1x_), state{}, codec(std::make_unique<Codec>(host1x, state)) {}
+
+Nvdec::~Nvdec() = default;
+
+void Nvdec::ProcessMethod(u32 method, u32 argument) {
+    state.reg_array[method] = static_cast<u64>(argument) << 8;
+
+    switch (method) {
+    case NVDEC_REG_INDEX(set_codec_id):
+        codec->SetTargetCodec(static_cast<NvdecCommon::VideoCodec>(argument));
+        break;
+    case NVDEC_REG_INDEX(execute):
+        Execute();
+        break;
     }
+}
+
+AVFramePtr Nvdec::GetFrame() {
+    return codec->GetCurrentFrame();
+}
+
+void Nvdec::Execute() {
+    switch (codec->GetCurrentCodec()) {
+    case NvdecCommon::VideoCodec::H264:
+    case NvdecCommon::VideoCodec::VP8:
+    case NvdecCommon::VideoCodec::VP9:
+        codec->Decode();
+        break;
+    default:
+        UNIMPLEMENTED_MSG("Codec {}", codec->GetCurrentCodecName());
+        break;
+    }
+}
+
 }
